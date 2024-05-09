13f5c90f702e30205c471a5f43badcaeb28a9c17
kkongdo <kkongdoes@naver.com>
2024-05-09 16:48:03 +0900

    fix: last time

--- src/main/java/com/example/demo/Demo2Application.java
+++ src/main/java/com/example/demo/Demo2Application.java
@@ -5,10 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Demo2Application {
-
-
-
-
     public static void main(String[] args) {
         SpringApplication.run(Demo2Application.class, args);
     }
